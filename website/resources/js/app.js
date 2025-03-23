81c377e81e298112ea064846d1123618d39e1b04
Liangrun Da <liangrun.da@tum.de>
2025-03-23 09:14:13 +0800

    fix domain

--- website/resources/js/app.js
+++ website/resources/js/app.js
@@ -31,9 +31,9 @@ function selectCompletionScenePill(scenePill) {
     scene = scenePill.getAttribute("data-value");
     var ourVideo = document.getElementById("sceneCompletionOurs");
     var compVideo = document.getElementById("sceneCompletion2dgs");
-    ourVideo.src = "https://genapi.sibowu.com/resources/videos/completion/ours/" + scene + ".mp4";
+    ourVideo.src = "https://genapi.sibowu.com/website/resources/videos/completion/ours/" + scene + ".mp4";
     ourVideo.load();
-    compVideo.src = "https://genapi.sibowu.com/resources/videos/completion/2dgs/" + scene + ".mp4";
+    compVideo.src = "https://genapi.sibowu.com/website/resources/videos/completion/2dgs/" + scene + ".mp4";
     compVideo.load();
 }
 
@@ -49,10 +49,10 @@ function selectGenerationScenePill(scenePill) {
     var generatedRgb = document.getElementById("generatedRgb");
     var artifactDepth = document.getElementById("artifactDepth");
     var generatedDepth = document.getElementById("generatedDepth");
-    artifactRgb.src = "https://genapi.sibowu.com/resources/videos/generation/" + scene + "/artifact_rgb.mp4";
-    generatedRgb.src = "https://genapi.sibowu.com/resources/videos/generation/" + scene + "/generated_rgb.mp4";
-    artifactDepth.src = "https://genapi.sibowu.com/resources/videos/generation/" + scene + "/artifact_depth.mp4";
-    generatedDepth.src = "https://genapi.sibowu.com/resources/videos/generation/" + scene + "/generated_depth.mp4";
+    artifactRgb.src = "https://genapi.sibowu.com/website/resources/videos/generation/" + scene + "/artifact_rgb.mp4";
+    generatedRgb.src = "https://genapi.sibowu.com/website/resources/videos/generation/" + scene + "/generated_rgb.mp4";
+    artifactDepth.src = "https://genapi.sibowu.com/website/resources/videos/generation/" + scene + "/artifact_depth.mp4";
+    generatedDepth.src = "https://genapi.sibowu.com/website/resources/videos/generation/" + scene + "/generated_depth.mp4";
     artifactRgb.load();
     generatedRgb.load();
     artifactDepth.load();
@@ -74,9 +74,9 @@ function selectSparseCompVideo(scenePill, viewNum) {
     scene = scenePill.getAttribute("data-value");
     var ourVideo = document.getElementById("ourSparseVideo");
     var compVideo = document.getElementById("compSparseVideo");
-    ourVideo.src = "https://genapi.sibowu.com/resources/videos/sparse/ours/" + scene + ".mp4";
+    ourVideo.src = "https://genapi.sibowu.com/website/resources/videos/sparse/ours/" + scene + ".mp4";
     ourVideo.load();
-    compVideo.src = "https://genapi.sibowu.com/resources/videos/sparse/2dgs" + "/" + scene + ".mp4";
+    compVideo.src = "https://genapi.sibowu.com/website/resources/videos/sparse/2dgs" + "/" + scene + ".mp4";
     compVideo.load();
 
     var numViews = document.getElementById("num-views-sparse");
@@ -105,12 +105,12 @@ function selectCompVideo(methodPill, scenePill) {
     var ourVideo = document.getElementById("ourVideo");
     var compVideo = document.getElementById("compVideo");
     var inputVideo = document.getElementById("inputVideo");
-    ourVideo.src = "https://genapi.sibowu.com/resources/videos/dl3dv/ours/" + scene + ".mp4";
+    ourVideo.src = "https://genapi.sibowu.com/website/resources/videos/dl3dv/ours/" + scene + ".mp4";
     ourVideo.load();
-    compVideo.src = "https://genapi.sibowu.com/resources/videos/dl3dv/" + method + "/" + scene + ".mp4";
+    compVideo.src = "https://genapi.sibowu.com/website/resources/videos/dl3dv/" + method + "/" + scene + ".mp4";
     compVideo.setAttribute("vc-caption", method.toUpperCase());
     compVideo.load();
-    inputVideo.src = "https://genapi.sibowu.com/resources/videos/dl3dv/input/" + scene + ".mp4";
+    inputVideo.src = "https://genapi.sibowu.com/website/resources/videos/dl3dv/input/" + scene + ".mp4";
     inputVideo.load();
     inputVideo.setAttribute("vc-caption", "Input");
 }
